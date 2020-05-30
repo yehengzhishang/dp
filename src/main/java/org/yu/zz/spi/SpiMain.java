f1ce423f7ba1f1b7038578bf9b7eb0456bb20736
zhaozhe <yehengzhishang@163.com>
2020-05-30 11:17:05 +0800

    更新注释 及 api

--- src/main/java/org/yu/zz/spi/SpiMain.java
+++ src/main/java/org/yu/zz/spi/SpiMain.java
@@ -1,18 +1,16 @@
 package org.yu.zz.spi;
 
-import sun.misc.Service;
-
-import java.util.Iterator;
 import java.util.ServiceLoader;
 
 public class SpiMain {
     public static void main(String[] args) {
-        Iterator<SpiService> providers = Service.providers(SpiService.class);
+        // 不建议，建议不使用
+//        Iterator<SpiService> providers = Service.providers(SpiService.class);
+//        while (providers.hasNext()) {
+//            providers.next().run();
+//        }
+//        System.out.println("====================");
         ServiceLoader<SpiService> load = ServiceLoader.load(SpiService.class);
-        while (providers.hasNext()) {
-            providers.next().run();
-        }
-        System.out.println("====================");
         for (SpiService spiService : load) {
             spiService.run();
         }
