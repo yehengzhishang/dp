09846cce16853ce7a255896ffdfb97807bc7fe52
yehengzhishang <yehengzhishang@163.com>
2020-07-22 07:27:08 +0800

    最小数

---
+++ src/main/java/org/yu/zz/algorithm/LeetCode.java
@@ -0,0 +1,24 @@
+package org.yu.zz.algorithm;
+
+public class LeetCode {
+
+    /**
+     * 剑指 Offer 11. 旋转数组的最小数字
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int minArray(int[] numbers) {
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            int num = numbers[i];
+            if (num < min) {
+                min = num;
+                break;
+            }
+        }
+        return min;
+    }
+}
